9091cde6c1b6daea405e83e7efae3af6c5bb91ac
Nicholas Dean <NicholasDean123@gmail.com>
2020-09-20 20:23:16 -0400

    updated routes and html templates

--- models.py
+++ models.py
@@ -12,22 +12,32 @@ def connect_db(app):
 
 class User(db.Model):
     __tablename__ ='users'
-    username = db.Column(db.Text(20), primary_key=True, nullable=False, unique=True)
+    username = db.Column(db.String(20), primary_key=True, nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
-    email = db.Column(db.Text(50), nullable=False, unique=True)
-    first_name = db.Column(db.Text(30), nullable=False)
-    last_name = db.Column(db.Text(30), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+    feedback = db.relationship("Feedback", backref="user", cascade="all,delete")
+
+    # start of convenience class methods
 
     @classmethod
-    def register(cls, username, pwd):
-        """ Register user w/hashed password and return user"""
+    def register(cls, username, password, first_name, last_name, email):
+        """Register a user, hashing their password."""
 
-        hashed = bcrypt.generate_password_hash(pwd)
-        # turn bytestring into normal (unicode utf8) string
-        hashed_utf8 = hashed.decode("utf8") 
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
 
-        # return instance of user w/username and hashed pwd
-        return cls(username=username, password=hashed_utf8)
+        db.session.add(user)
+        return user
 
     @classmethod
     def authenticate(cls, username, pwd):
@@ -40,4 +50,18 @@ class User(db.Model):
             return u
         else: 
             return False
+
+class Feedback(db.Model):
+    """Feedback."""
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(
+        db.String(20),
+        db.ForeignKey('users.username'),
+        nullable=False,
+    )
 
